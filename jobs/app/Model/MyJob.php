61118d7da4ebce97e0d2621a7b6c6c0f1e58c3d0
DamianGallego <damiangallego09@gmail.com>
2019-04-16 17:26:58 -0300

    Se creo algoritmo para determinar la duracion de un job

--- jobs/app/Model/MyJob.php
+++ jobs/app/Model/MyJob.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\Model;
 
 abstract class MyJob extends Model{
 
+    //Variable queue si viene por aparametro meta el queue por default.
     protected $table = "jobs_events";
 
     abstract function getType();
@@ -24,6 +25,7 @@ abstract class MyJob extends Model{
     const CREATED_AT = false;
 
     const UPDATED_AT = false;
+
 
 
 }
--- jobs/database/migrations/2019_04_15_024210_create_jobs_events_table.php
+++ jobs/database/migrations/2019_04_15_024210_create_jobs_events_table.php
@@ -18,8 +18,10 @@ class CreateJobsEventsTable extends Migration
             $table->string('queue')->index();
             $table->string('name');
             $table->string('job');
+            $table->string('duration');
+            $table->boolean('fullscan');
             $table->timestamp('start')->default(\Carbon\Carbon::now());
-            $table->timestamp('finish')->nullable();
+            $table->timestamp('finish')->default(\Carbon\Carbon::now()->addSeconds(5));
             $table->integer('status');
             $table->unsignedTinyInteger('attempts');
         });
