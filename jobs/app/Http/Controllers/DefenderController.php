72663388a5571021d01576c23974b4c780c77e26
DamianGallego <damiangallego09@gmail.com>
2019-04-22 09:57:05 -0300

    Merge branch 'develop' of https://github.com/fedeberon/jobsChallenge into develop
    # Conflicts:
    #	jobs/app/Model/Defender.php

--- jobs/app/Http/Controllers/DefenderController.php
+++ jobs/app/Http/Controllers/DefenderController.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Application;
 use App\Jobs\ProcessJobs;
 use App\Model\Defender;
-use App\Model\Operative;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
 use Validator;
 
--- jobs/config/app.php
+++ jobs/config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \Http\Services\JobServiceImpl::class,
 
     ],
 
